73639879b6c674f51e4a70fb1758cb76984121ab
Aidan Gallagher <aidan@cloudflare.com>
2024-10-04 13:21:37 +0100

    WIP: delete home directory and select bash as default shell

--- files/image_config/local-login/local-login.py
+++ files/image_config/local-login/local-login.py
@@ -4,6 +4,8 @@ import subprocess
 import json
 import pwd
 
+# sonic-cfggen -d --var-json 'LOCAL_LOGIN'
+
 result = subprocess.run(['sonic-cfggen', '-d', '--var-json', 'LOCAL_LOGIN'], text=True, capture_output=True).stdout
 print(result)
 result_dict = json.loads(result)
@@ -18,7 +20,7 @@ for user in result_dict:
 		pass
 	else:
 		print("creating user")
-		subprocess.run(['sudo', 'useradd', '--create-home', user], text=True, capture_output=True)
+		subprocess.run(['sudo', 'useradd', '--create-home', '--shell', '/bin/bash', user], text=True, capture_output=True)
 
 	subprocess.run(['sudo', 'usermod', '-aG', 'sudo', user], text=True, capture_output=True)
 
@@ -35,4 +37,6 @@ for p in pwd.getpwall():
 for user in users:
 	if user not in result_dict:
 		# Force delete the user in case they are logged in. The CLI command will do the warning that the user is logged in
+		# Remove home directory so if operator creates a new user with the same name there won't be problems.
 		subprocess.run(['sudo', 'userdel', '--force', user], check=True)
+		subprocess.run(['sudo', 'rm', '-rf', f'/home/{user}'], check=True)
